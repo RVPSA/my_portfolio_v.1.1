8c1ea6bc2ecba1ca2c125e6498e984ddeefc18ef
RVPSA <akeshramasinghe@gmail.com>
2025-01-24 22:00:50 +0000

    Initializing react vite project with tailwindcss and make first component 'SkillCard'

---
+++ my_portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SkillCard from './components/SkillCard'
+
+function App() {
+  return(
+    <>
+    <div className='bg-[#282c33] p-10'><SkillCard></SkillCard></div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ my_portfolio/src/components/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+  return (
+    <div className="border border-[#775288] px-2 py-1 text-white text-sm">Live</div>
+  )
+}
+
+export default Button
