1aaa7c87669efc660b2db31df30fd92635149c7d
RVPSA <akeshramasinghe@gmail.com>
2025-01-27 21:25:31 +0000

    Develop component for button, have to do more enhancements

--- my_portfolio/src/components/Button.tsx
+++ my_portfolio/src/components/Button.tsx
@@ -1,7 +1,21 @@
+//@ts-ignore
+import ClickSpark from "../sideSupportFiles/Animations/ClickSpark/ClickSpark.jsx";
 const Button = () => {
   return (
-    <div className="border border-[#775288] px-2 py-1 text-white text-sm">Live</div>
-  )
-}
+    // <div className="border border-[#775288] px-2 py-1 text-white text-sm">Live</div>
+    <>
+      <div
+        className="
+        border border-[#775288] px-2 py-1 text-white text-sm text-center
+        "
+        onClick={() => {
+          console.log("ABC");
+        }}
+      >
+        <button>Live</button>
+      </div>
+    </>
+  );
+};
 
-export default Button
+export default Button;
