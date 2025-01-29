c089532f7bd5a7e0c1778b5a36d0895f97764baa
RVPSA <akeshramasinghe@gmail.com>
2025-01-29 17:11:05 +0100

    added some assets

--- my_portfolio/src/assets/index.tsx
+++ my_portfolio/src/assets/index.tsx
@@ -1,7 +1,9 @@
 import menu from "./menu.svg";
 import close from "./close.svg";
+import firstComma from "./firstcomma.svg";
+import lastComma from "./lastComma.svg";
+import akeshImage from './AkeshImage.png'
+import dots from './Dots.png';
+import design from './Style=Outline.png'
 
-export {
-  menu,
-  close
-};
+export { menu, close, firstComma, lastComma,akeshImage,dots,design };
