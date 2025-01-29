bfc221235569312e7c200b5f9a5b831aab1e68b2
RVPSA <akeshramasinghe@gmail.com>
2025-01-29 10:08:47 +0100

    Added proper commenting style to code

--- my_portfolio/src/components/Button.tsx
+++ my_portfolio/src/components/Button.tsx
@@ -1,8 +1,22 @@
 //@ts-ignore
-import ClickSpark from "./ClickSpark.jsx";
+import ClickSpark from "../sideSupportComponent/ClickSpark.jsx";
 
-const Button = () => {
+/**
+ * Button Component of project with responsiveness, using ClickSpark component
+ * * Important
+ * ! Alert
+ * ? Should use
+ * TODO: Todo code
+ * <> Dev comment
+ * @param text : String, text for the button
+ * @param onClick : void function, button for the function
+ */
 
+type ButtonProps = {
+  text: string;
+  onClick: () => void;
+};
+const Button = (prop: ButtonProps) => {
   return (
     <>
       <div
@@ -10,12 +24,14 @@ const Button = () => {
         border border-[#775288]  py-1 text-white text-sm text-center
         relative cursor-pointer w-16 h-6
         STablet:w-32"
-        onClick={() => {
-          console.log("ABC");
-        }}
+        onClick={prop.onClick}
       >
-        <h1 className="absolute inset-0 z-0">Live</h1>
-        <div className="absolute h-full w-full top-0 left-0"><ClickSpark></ClickSpark></div>
+        {/* //<> Text of the button, as the lower layer of the button div */}
+        <h1 className="absolute inset-0 z-0">{prop.text}</h1>
+        {/* //<>Use ClickSpark component, as the top layer of the button div */}
+        <div className="absolute h-full w-full top-0 left-0">
+          <ClickSpark></ClickSpark>
+        </div>
       </div>
     </>
   );
