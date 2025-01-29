525bc7d46a921f4f5941987ffd3c624e21a7172b
RVPSA <akeshramasinghe@gmail.com>
2025-01-29 17:12:26 +0100

    added feature to change text on small screen

--- my_portfolio/src/components/Button.tsx
+++ my_portfolio/src/components/Button.tsx
@@ -14,6 +14,7 @@ import ClickSpark from "../sideSupportComponent/ClickSpark.jsx";
 
 type ButtonProps = {
   text: string;
+  shortText?: string;
   onClick: () => void;
 };
 const Button = (prop: ButtonProps) => {
@@ -26,8 +27,15 @@ const Button = (prop: ButtonProps) => {
         STablet:w-32"
         onClick={prop.onClick}
       >
-        {/* //<> Text of the button, as the lower layer of the button div */}
-        <h1 className="absolute inset-0 z-0">{prop.text}</h1>
+        {/* //<> Text of the button, as the lower layer of the button div 
+        // //<>First part on normal screen, second part on mobile screen whith short text
+        // */}
+        <h1 className="hidden STablet:flex absolute inset-0 z-0 pl-1">
+          {prop.text}
+        </h1>
+        <h1 className="flex STablet:hidden absolute inset-0 z-0 pl-1">
+          {prop.shortText != "" ? prop.shortText : prop.text}
+        </h1>
         {/* //<>Use ClickSpark component, as the top layer of the button div */}
         <div className="absolute h-full w-full top-0 left-0">
           <ClickSpark></ClickSpark>
