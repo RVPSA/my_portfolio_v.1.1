8440c60ca5f8fc02056276f0cd2f3805940652fe
RVPSA <akeshramasinghe@gmail.com>
2025-01-30 09:59:31 +0100

    Designing skill card component that shows the skill type and skills

---
+++ my_portfolio/src/components/SkillCard.tsx
@@ -0,0 +1,24 @@
+type SkillCardProps = {
+  skillType: string;
+  skills: string[];
+};
+/**
+ * Skill showcase component
+ * @param SkillCardProps
+ */
+const SkillCard = (prop: SkillCardProps) => {
+  return (
+    <>
+      <div className="text-white border-[#494d56] border-2 w-fit">
+        <div className="border-[#494d56] border-b-2 p-2">Languages</div>
+        <div className="p-2">
+          {prop.skills.map((data) => {
+            return <div>{data}</div>;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkillCard;
