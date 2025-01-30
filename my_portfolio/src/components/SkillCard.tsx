4f86cc9ed2a91a8df3a8abbb3568368be8830139
RVPSA <akeshramasinghe@gmail.com>
2025-01-30 15:13:23 +0100

    skill card bug fixing

--- my_portfolio/src/components/SkillCard.tsx
+++ my_portfolio/src/components/SkillCard.tsx
@@ -9,7 +9,7 @@ type SkillCardProps = {
 const SkillCard = (prop: SkillCardProps) => {
   return (
     <>
-      <div className="text-white border-[#494d56] border-2 w-fit">
+      <div className="text-white border-[#494d56] border-2 w-full">
         <div className="border-[#494d56] border-b-2 p-2">Languages</div>
         <div className="p-2">
           {prop.skills.map((data) => {
