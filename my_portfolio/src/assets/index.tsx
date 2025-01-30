d8cdfa783aee286469e96ac8a6494b6018e899dc
RVPSA <akeshramasinghe@gmail.com>
2025-01-30 15:11:48 +0100

    added some asests

--- my_portfolio/src/assets/index.tsx
+++ my_portfolio/src/assets/index.tsx
@@ -2,8 +2,26 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import firstComma from "./firstcomma.svg";
 import lastComma from "./lastComma.svg";
-import akeshImage from './AkeshImage.png'
-import dots from './Dots.png';
-import design from './Style=Outline.png'
+import akeshImage from "./AkeshImage.png";
+import dots from "./Dots.png";
+import design from "./Style=Outline.png";
+import linkedin from "./Linkedin.svg";
+import email from "./Email.svg";
+import github from "./Github.svg";
+import whatsapp from './whatsapp.svg'
+import call from './call.svg'
 
-export { menu, close, firstComma, lastComma,akeshImage,dots,design };
+export {
+  menu,
+  close,
+  firstComma,
+  lastComma,
+  akeshImage,
+  dots,
+  design,
+  linkedin,
+  email,
+  github,
+  whatsapp,
+  call
+};
