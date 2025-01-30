6310264a239dd2e133c387eb979b3370d0a772b9
RVPSA <akeshramasinghe@gmail.com>
2025-01-30 18:38:53 +0100

    layout changing

--- my_portfolio/src/components/Button.tsx
+++ my_portfolio/src/components/Button.tsx
@@ -14,26 +14,26 @@ import ClickSpark from "../sideSupportComponent/ClickSpark.jsx";
 
 type ButtonProps = {
   text: string;
-  shortText?:string;
+  shortText?: string;
   onClick: () => void;
+  isborder?: boolean;
 };
 const Button = (prop: ButtonProps) => {
   return (
     <>
       <div
-        className="
-        border border-[#775288]  py-1 text-white text-sm text-center
+        className={`${prop.isborder != false && "border border-[#775288]"} py-1 text-white text-sm text-center
         relative cursor-pointer w-16 h-6
-        STablet:w-32"
+        STablet:w-32 STablet:h-7`}
         onClick={prop.onClick}
       >
         {/* //<> Text of the button, as the lower layer of the button div 
         // //<>First part on normal screen, second part on mobile screen whith short text
         // */}
-        <h1 className="hidden STablet:flex absolute inset-0 z-0 pl-1">
+        <h1 className="hidden STablet:flex absolute inset-0 z-0 pl-1 pt-1">
           {prop.text}
         </h1>
-        <h1 className="flex STablet:hidden absolute inset-0 z-0 pl-1">
+        <h1 className="flex STablet:hidden absolute inset-0 z-0 pl-1 pt-1">
           {prop.shortText != null ? prop.shortText : prop.text}
         </h1>
         {/* //<>Use ClickSpark component, as the top layer of the button div */}
