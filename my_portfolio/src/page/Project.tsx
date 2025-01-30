21c75f55e49db94c71f508c213d4263780dd74f6
RVPSA <akeshramasinghe@gmail.com>
2025-01-30 16:06:50 +0100

    alogn modification pf project card

--- my_portfolio/src/page/Project.tsx
+++ my_portfolio/src/page/Project.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ProjectCard from "../components/ProjectCard";
 /**
  * Projec page, That shows the completed project
@@ -9,22 +8,26 @@ const Project = () => {
       <div className="grid grid-cols-12 pt-12 gap-5">
         {/* //<> Header section */}
         <div className="col-span-12 grid grid-cols-12 items-center pb-9">
-          <div className="col-span-4 Laptop:col-span-2 text-white"><div className="w-full">#Projects</div></div>
-          <div className="h-[2px] bg-[#775288] col-span-4 Laptop:col-span-5"></div>
-          <div className="col-span-3 Laptop:col-span-4 grid justify-end Laptop:pr-11">
-            <div className="text-white">Viewing </div>
+          <div className="col-span-12 text-white">
+            <div className="w-full text-xl font-semibold">
+              <span className="text-[#775288] text-2xl font-bold">/</span>
+              Projects
+            </div>
+          </div>
+          <div className="col-span-12">
+            <div className="text-slate-400">List of my projects </div>
           </div>
         </div>
-            {/* //<> Project card section */}
-          <div className="col-span-12 STablet:col-span-6 Laptop:col-span-4 grid justify-center">
-            <ProjectCard></ProjectCard>
-          </div>
-          <div className="col-span-12 STablet:col-span-6 Laptop:col-span-4 grid justify-center">
-            <ProjectCard></ProjectCard>
-          </div>
-          <div className="col-span-12 STablet:col-span-6 Laptop:col-span-4 grid justify-center">
-            <ProjectCard></ProjectCard>
-          </div>
+        {/* //<> Project card section */}
+        <div className="col-span-12 STablet:col-span-6 Laptop:col-span-4 grid justify-start">
+          <ProjectCard></ProjectCard>
+        </div>
+        <div className="col-span-12 STablet:col-span-6 Laptop:col-span-4 grid justify-start">
+          <ProjectCard></ProjectCard>
+        </div>
+        <div className="col-span-12 STablet:col-span-6 Laptop:col-span-4 grid justify-start">
+          <ProjectCard></ProjectCard>
+        </div>
       </div>
     </>
   );
