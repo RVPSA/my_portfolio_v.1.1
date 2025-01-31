df39cfcf7f9352bbe723cba8e30f0682ec19c408
RVPSA <akeshramasinghe@gmail.com>
2025-01-31 14:25:54 +0100

    Bug fixing and optimizing

--- my_portfolio/src/components/SkillCard.tsx
+++ my_portfolio/src/components/SkillCard.tsx
@@ -10,10 +10,10 @@ const SkillCard = (prop: SkillCardProps) => {
   return (
     <>
       <div className="text-white border-[#494d56] border-2 w-full">
-        <div className="border-[#494d56] border-b-2 p-2">Languages</div>
+        <div className="border-[#494d56] border-b-2 p-2">{prop.skillType}</div>
         <div className="p-2">
-          {prop.skills.map((data) => {
-            return <div>{data}</div>;
+          {prop.skills.map((data,index) => {
+            return <div key={index}>{data}</div>;
           })}
         </div>
       </div>
