6c41567f802120f9417e1ebd3d39f11760956c98
RVPSA <akeshramasinghe@gmail.com>
2025-01-31 14:24:47 +0100

    added images to icon scroll section

--- my_portfolio/src/assets/index.tsx
+++ my_portfolio/src/assets/index.tsx
@@ -8,8 +8,24 @@ import design from "./Style=Outline.png";
 import linkedin from "./Linkedin.svg";
 import email from "./Email.svg";
 import github from "./Github.svg";
-import whatsapp from './whatsapp.svg'
-import call from './call.svg'
+import whatsapp from "./whatsapp.svg";
+import call from "./call.svg";
+import javascript from "./javascript.svg";
+import reactjs from "./reactjs.svg";
+import sql from "./sql-file.svg";
+import tailwind from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+import git from "./github.svg";
+import dotnet from "./dot-net.svg";
+import csharp from "./c-sharp.svg";
+import docker from "./docker.svg";
+import kubernetes from "./kubernetes.svg";
+import portfolio from "./portfolio.png";
+import grpc from "./grpc.png";
+import keells from "./keels.png";
+import deeghayuresort from './homeweb.png'
+import subtitle from "./subtitle.png";
+import schoolManagement from "./schoolmanagement.png"
 
 export {
   menu,
@@ -23,5 +39,21 @@ export {
   email,
   github,
   whatsapp,
-  call
+  call,
+  javascript,
+  reactjs,
+  sql,
+  tailwind,
+  typescript,
+  git,
+  dotnet,
+  csharp,
+  docker,
+  kubernetes,
+  portfolio,
+  grpc,
+  keells,
+  deeghayuresort,
+  subtitle,
+  schoolManagement
 };
